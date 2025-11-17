27c0d3d126169a64e9fcd2919c0ad7a5032f17d3
EdgarFav <edgarfda17@gmail.com>
2025-11-16 19:17:07 -0600

    feat: add backend and frontend Docker configurations with module structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SalesDataModule } from './sales-data/sales-data.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, SalesDataModule],
   controllers: [AppController],
   providers: [AppService],
 })
