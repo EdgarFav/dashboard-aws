106275f5348e7625d8e52aed5c8054be1380707b
EdgarFav <edgarfda17@gmail.com>
2025-11-20 14:22:50 -0600

    feat: habilitar polling en Docker para backend y frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), checker({typescript: true}), tailwindcss()],
-  
+   server: {
+    host: true, // o '0.0.0.0'
+    port: 5173,
+    watch: {
+      usePolling: true, // Importante para Docker en Windows
+    }
+  }
 })
