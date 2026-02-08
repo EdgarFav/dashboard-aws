a2e8d66f33ea87ecc9e1ee2fe22a1a0b41e0d750
EdgarFav <edgarfda17@gmail.com>
2026-02-07 23:37:15 -0600

    feat: Implement basic user management with TypeORM and configure initial MySQL database connection.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,15 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'mysql_db',
+      // host: 'localhost',
       port: 3306,
+      // port: 3307,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
       username: 'user',
