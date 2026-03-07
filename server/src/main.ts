636ea329570d3e04d291f463431da807a9676bc6
EdgarFav <edgarfda17@gmail.com>
2026-03-07 11:56:27 -0600

    update: corregir la URL de origen en la configuración de CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   // Configuración de CORS
   app.enableCors({
     // origin: 'http://localhost:5173', // El puerto de tu cliente Vite
-    origin: 'https://dashboard-aws-eight.vercel.app/',
+    origin: 'https://dashboard-aws-eight.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
